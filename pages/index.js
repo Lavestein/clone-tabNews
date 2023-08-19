d78fd090d7a88f15fb1f36efb6b3e9d03ce40485
Ronaldo Lavestein <71901765+Lavestein@users.noreply.github.com>
2023-08-19 10:21:45 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 function Home() {
-  return <h1>Clone do tabNews.com - curso.dev - Filipe Deschamps 2023</h1>
+  return <h1>Clone do tabNews.com - curso.dev - Filipe Deschamps 2023</h1>;
 }
 
-function Teste() {
-  console.log("Indentação errada");
-}
 export default Home;
